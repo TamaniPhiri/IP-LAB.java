a9d0fa67ffe668f4301015fa552dc8fba42ce786
tamani phiri <tamanigabriel0@gmail.com>
2023-11-29 14:49:47 +0200

    c

---
+++ DownloadHompageAndSetUrl/Main.java
@@ -0,0 +1,29 @@
+package DownloadHompageAndSetUrl;
+import java.io.*;
+import java.net.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String url="https://www.google.com";
+
+        try {
+            String content= readUrl(url);
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readUrl(String urlString) throws IOException{
+        StringBuilder content=new StringBuilder();
+        URL url =new URL(urlString);
+        try(BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()))){
+            String line;
+            while((line=reader.readLine())!=null){
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
+}
