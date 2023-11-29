c70fb86a3f186782291a8d324949a4e8a731c03a
tamani phiri <tamanigabriel0@gmail.com>
2023-11-29 14:50:14 +0200

    c

--- DownloadHompageAndSetUrl/Main.java
+++ DownloadHompageAndSetUrl/Main.java
@@ -1,26 +1,26 @@
 package DownloadHompageAndSetUrl;
+
 import java.io.*;
 import java.net.*;
 
-
 public class Main {
     public static void main(String[] args) {
-        String url="https://www.google.com";
+        String url = "https://www.google.com";
 
         try {
-            String content= readUrl(url);
+            String content = readUrl(url);
             System.out.println(content);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String readUrl(String urlString) throws IOException{
-        StringBuilder content=new StringBuilder();
-        URL url =new URL(urlString);
-        try(BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()))){
+    private static String readUrl(String urlString) throws IOException {
+        StringBuilder content = new StringBuilder();
+        URL url = new URL(urlString);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
             String line;
-            while((line=reader.readLine())!=null){
+            while ((line = reader.readLine()) != null) {
                 content.append(line).append("\n");
             }
         }
